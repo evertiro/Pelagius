4295df72df98d318ddc7b5c34a320a0be648fa58
Ryan Henderson <ryan.ev.henderson@gmail.com>
2020-10-09 16:34:59 -0500

    Remove unused config dependency

--- pelagius.js
+++ pelagius.js
@@ -3,7 +3,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const https = require('https');
-const config = require('config');
 
 const token = process.env.BOT_TOKEN;
 
